112f9ddcd74c8f67488f404a012b623f5561a6f4
Michael Bushoy <mbushoy@gmail.com>
2016-06-29 17:08:52 -0400

    chart moved to ctrl, taking in px/position data

--- client/components/dashboard/dashboard.controller.js
+++ client/components/dashboard/dashboard.controller.js
@@ -32,29 +32,31 @@
           vm.pxStream[type].push([Date.now(),px]) //track prices
 
           if (type === 'buy') {
+            vm.trades.push({t: new Date(), px, q}); //track trade history
             lots.push({t: new Date(), px, q}); //track lots
+            vm.position += q;
           }
           else {
-            q*=-1;
+            vm.trades.push({t: new Date(), px, q: -q}); //track trade history
             //TODO: short lot tracking
+            vm.position -= q;
           }
-          vm.trades.push({t: new Date(), px, q}); //track trade history
-          vm.position += q;
+          updateChart();
           recalcPNL();
         })
         .catch(err => console.log(err))
-
       }
 
-      // setInterval(priceUpdate,10000)
+      setInterval(priceUpdate,10000)
       function priceUpdate() {
-        console.log("update")
+        console.log("updating...")
         DashFactory.getTradePx('spot')
         .then(px => {
           if (Number(px) !== vm.spot) {
             vm.pxStream.spot.push([Date.now(),px])
             vm.spot = px;
             console.log(vm.pxStream)
+            updateChart();
             recalcPNL();
           } else {
             console.log("no new price available")
@@ -71,5 +73,71 @@
         vm.unrealPNL = vm.position * vm.spot - costBasis;
       }
 
-    }
+      function updateChart() {
+        vm.data[0].values.push({x: Date.now(), y: vm.position}) //update graph
+        vm.data[1].values.push({x: Date.now(), y: vm.spot}) //update graph
+      }
+
+      //chart data
+      vm.data = [
+        {
+          "key" : "Quantity" ,
+          "bar": true,
+          "values" : [[Date.now(), 0] ]
+        },
+        {
+          "key" : "BTC Price" ,
+          "values" : [[Date.now(), vm.spot]]
+        }
+      ].map(function(series) {
+          series.values = series.values.map(function(d) { return { x: d[0], y: d[1] } });
+          return series;
+        });
+
+      vm.options = {
+        chart: {
+          type: 'linePlusBarChart',
+          height: 500,
+          margin: {
+              top: 30,
+              right: 75,
+              bottom: 50,
+              left: 75
+          },
+          bars: {
+              forceY: [-50,100]
+          },
+          lines: {
+            forceY: [vm.spot-25,vm.spot+25]
+          },
+          color: ['grey', 'darkred'],
+          x: function(d,i) { return i },
+          focusEnable: false,
+          xAxis: {
+              axisLabel: 'X Axis',
+              tickFormat: function(d) {
+                var dx = vm.data[1].values[d] && vm.data[1].values[d].x || 0;
+                if (dx > 0) {
+                    return d3.time.format('%X')(new Date(dx))
+                }
+                return null;
+              }
+          },
+          y1Axis: {
+              axisLabel: 'Quantity',
+              tickFormat: function(d){
+                return d3.format(',f')(d);
+              },
+              axisLabelDistance: 12
+          },
+          y2Axis: {
+              axisLabel: 'Price',
+              tickFormat: function(d) {
+                return '$' + d3.format(',.2f')(d)
+            }
+          }
+        }
+      }
+}
+
 })();
